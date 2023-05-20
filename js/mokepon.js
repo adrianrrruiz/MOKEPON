7a9a19e8225cb4fe7063c019ab185b2fedf8ba4a
Adrian Ruiz <adrianruiz0208@gmail.com>
2023-05-19 22:12:17 -0500

    Mejorando la funcion seleccionarMascotaEnemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -120,15 +120,9 @@ function seleccionarMascotaJugador(){
 }
 
 function seleccionarMascotaEnemigo(){
-    let mascotaAleatorio = aleatorio(1,3)
+    let mascotaAleatorio = aleatorio(0, mokepones.length - 1)
 
-    if(mascotaAleatorio == 1){
-        spanMascotaEnemigo.innerHTML = 'Hipodoge'
-    }else if(mascotaAleatorio == 2){
-        spanMascotaEnemigo.innerHTML = 'Capipepo'
-    }else if(mascotaAleatorio == 3){
-        spanMascotaEnemigo.innerHTML = 'Ratigueya'
-    }
+    spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatorio].nombre
 }
 
 function ataqueFuego(){
