58c98b3deb3b5edcf480fab9212489b7e5dd422e
Adrian Ruiz <adrianruiz0208@gmail.com>
2023-05-26 17:21:33 -0500

    Arreglando bug de los personajes

--- mokepon.js
+++ mokepon.js
@@ -22,6 +22,7 @@ const mapa = document.getElementById('mapa')
 
 let jugadorId = null
 let mokepones = []
+let mokeponesEnemigos = []
 let ataqueJugador = []
 let ataqueEnemigo = []
 let opcionDeMokepones
@@ -352,9 +353,9 @@ function pintarCanvas(){
 
     enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y)
 
-    hipodogeEnemigo.pintarMokepon()
-    capipepoEnemigo.pintarMokepon()
-    ratigueyaEnemigo.pintarMokepon()
+    mokeponesEnemigos.forEach(function (mokepon) {
+        mokepon.pintarMokepon()
+    })
 
     if(mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0){
         revisarColision(hipodogeEnemigo)
@@ -380,7 +381,7 @@ function enviarPosicion(x, y) {
                 res.json()
                     .then(function ({ enemigos }) {
                         console.log(enemigos)
-                        enemigos.forEach(function (enemigo){
+                        mokeponesEnemigos = enemigos.map(function (enemigo){
                             let mokeponEnemigo = null
                             const mokeponNombre = enemigo.mokepon.nombre || ""
                             if (mokeponNombre === "Hipodoge") {
@@ -394,7 +395,7 @@ function enviarPosicion(x, y) {
                             mokeponEnemigo.x = enemigo.x
                             mokeponEnemigo.y = enemigo.y
 
-                            mokeponEnemigo.pintarMokepon()
+                            return mokeponEnemigo
                         })
                     })
             }
